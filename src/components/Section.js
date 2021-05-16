1bef45c4c8a2c44e112adb276b0648a2f7661473
Aftab <aftabahamedvm@gmail.com>
2021-05-16 22:21:27 +0530

    added section component

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,16 +8,17 @@ function Section() {
                 <h1>Model S</h1>
                 <p>Order online for Touchless delivery</p>
             </ItemText>
-
-            <ButtonGroup>
-                <LeftButton>
-                    Custom Order
-                </LeftButton>
-                <RightButton>
-                    Existing Inventory
-                </RightButton>
-            </ButtonGroup>
-
+            <Buttons>
+                <ButtonGroup>
+                    <LeftButton>
+                        Custom Order
+                    </LeftButton>
+                    <RightButton>
+                        Existing Inventory
+                    </RightButton>
+                </ButtonGroup>
+                <DownArrow src="/images/down-arrow.svg" />
+            </Buttons>
         </Wrap>
     )
 }
@@ -31,16 +32,47 @@ const Wrap = styled.div`
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
 `
 
 const ItemText = styled.div`
     padding-top: 15vh;
     text-align: center;
 `
-const ButtonGroup = styled.div``
-
-const LeftButton = styled.div`
-    
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
 `
 
-const RightButton = styled(LeftButton)``
+const LeftButton = styled.div`
+    background-color: rgba(23,26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+    margin: 8px;
+`
+
+const RightButton = styled(LeftButton)`
+    background: white;
+    opacity: 0.65;
+    color: black;
+`
+
+const DownArrow = styled.img`
+    margin-top: 20px;
+    height: 40px;
+    animation: animateDown infinite 1.5s;
+`
+
+const Buttons = styled.div``
