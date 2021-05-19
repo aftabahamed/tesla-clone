0cad0c1122b928e4cad49967cb9fd6a7c4b52eb6
Aftab <aftab@Gopinaths-MacBook-Pro.local>
2021-05-19 15:41:03 +0530

    sections and json data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,28 @@
-import React from 'react'
-import styled from 'styled-components'
-import Section from './Section'
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+import Cars from "../json/cars.json";
 
 function Home() {
-    return (
-        <Container>
-            <Section />
-        </Container>
-    )
+  console.log(Cars);
+  return (
+    <Container>
+      {Cars.map((car, index) => (
+        <Section
+          key={index}
+          title={car.title}
+          description={car.description}
+          backgroundImg={car.backgroundImg}
+          leftBtnText={car.leftBtnText}
+          rightBtnText={car.rightBtnText}
+        />
+      ))}
+    </Container>
+  );
 }
 
-export default Home
+export default Home;
 
 const Container = styled.div`
-    height: 100vh;
-`
+  height: 100vh;
+`;
